915e951e31438574ae7b46314545bea1d9a07aad
Logan Dallalio <logan@dallalioweb.dev>
2022-12-19 23:23:54 -0600

    cors...

--- api/index.js
+++ api/index.js
@@ -12,6 +12,23 @@ const ejs = require('ejs');
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
+
+module.exports = (req, res) => {
+    // set header first to allow request or origin domain (value can be different)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+    // ---- other code
+
+    // Preflight CORS handler
+    if (req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: 'OK',
+        }));
+    }
+};
 const cors = require('cors');
 // const signUp = ;
 
